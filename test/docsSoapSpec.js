7272ef3043d8de68c3c1950f4b91cb006074c37c
Tomas Ruzicka <me@tomasr.com>
2016-07-07 17:34:24 +0200

    traverse list items fixes aem/docs-soap#2

--- test/docsSoapSpec.js
+++ test/docsSoapSpec.js
@@ -1,6 +1,6 @@
 'use es6';
 
-import { elements } from '../src/constants'
+import { elements } from '../src/constants';
 import documents from './fixtures/documents';
 import docsSoap from '../src/index';
 import expect from 'expect';
@@ -9,6 +9,13 @@ import parseHTML from '../src/parseHTML';
 
 describe('Google Docs Converter', () => {
   jsdom();
+
+  it('converts plain doc', () => {
+    const doc = docsSoap(documents.plain);
+    expect(doc).toExist();
+    const parsed = parseHTML(doc);
+    expect(parsed.textContent).toBe('Hello world.');
+  });
 
   it('converts inline styles from google docs properly', () => {
     const rawContents = parseHTML(documents.inlineStyles);
@@ -36,5 +43,59 @@ describe('Google Docs Converter', () => {
     expect(doc.querySelectorAll(elements.BOLD).length).toBe(2);
     expect(doc.querySelectorAll(elements.ITALIC).length).toBe(2);
     expect(doc.querySelectorAll(elements.ANCHOR).length).toBe(4);
+  });
+
+  it('converts nested list from google docs properly', () => {
+    const doc = parseHTML(docsSoap(documents.nestedList));
+    // ol
+    // - li
+    // - ol
+    //   - li
+    //   - ol
+    //     - li
+    // - li
+    // - ol
+    //   ....
+    // ul
+    // - li
+    // - li
+    // - ul
+    //   - li
+    //   - ul
+    //     - li
+    expect(doc.childNodes[0].nodeName).toBe('OL');
+    expect(doc.childNodes[0].childNodes.length).toBe(4);
+    expect(doc.childNodes[0].childNodes[0].nodeName).toBe('LI');
+    // jsdom bug: should actually include a line break
+    expect(doc.childNodes[0].childNodes[0].textContent).toBe('Abcd1234');
+    expect(doc.childNodes[0].childNodes[1].nodeName).toBe('OL');
+    expect(doc.childNodes[0].childNodes[1].childNodes.length).toBe(2);
+    expect(doc.childNodes[0].childNodes[1].childNodes[0].nodeName).toBe('LI');
+    expect(doc.childNodes[0].childNodes[1].childNodes[1].nodeName).toBe('OL');
+    expect(doc.childNodes[0].childNodes[1].childNodes[1].childNodes.length).toBe(1);
+    expect(doc.childNodes[0].childNodes[1].childNodes[1].childNodes[0].nodeName).toBe('LI');
+    expect(doc.childNodes[0].childNodes[2].nodeName).toBe('LI');
+    expect(doc.childNodes[0].childNodes[2].childNodes.length).toBe(1);
+    expect(doc.childNodes[0].childNodes[3].nodeName).toBe('OL');
+
+    expect(doc.childNodes[1].nodeName).toBe('UL');
+    expect(doc.childNodes[1].childNodes.length).toBe(3);
+    expect(doc.childNodes[1].childNodes[0].nodeName).toBe('LI');
+    expect(doc.childNodes[1].childNodes[1].nodeName).toBe('LI');
+    expect(doc.childNodes[1].childNodes[2].nodeName).toBe('UL');
+    expect(doc.childNodes[1].childNodes[2].childNodes[0].nodeName).toBe('LI');
+    expect(doc.childNodes[1].childNodes[2].childNodes[1].nodeName).toBe('UL');
+    expect(doc.childNodes[1].childNodes[2].childNodes[1].childNodes[0].nodeName).toBe('LI');
+    expect(doc.childNodes[1].childNodes[2].childNodes[1].childNodes[0].childNodes.length).toBe(1);
+  });
+
+  it('converts deep nested styles from gdocs properly', () => {
+    const doc = parseHTML(docsSoap(documents.nestedList));
+    expect(doc.childNodes[0].childNodes[0].querySelectorAll(elements.BOLD).length).toBe(3);
+    expect(doc.childNodes[0].childNodes[0].querySelectorAll(elements.UNDERLINE).length).toBe(3);
+    expect(doc.childNodes[0].childNodes[1].childNodes[1].childNodes[0].querySelectorAll(elements.ITALIC).length).toBe(1);
+    expect(doc.childNodes[0].childNodes[1].childNodes[1].childNodes[0].querySelector(elements.ITALIC).textContent).toBe('italics');
+    expect(doc.childNodes[0].childNodes[1].childNodes[1].childNodes[0].querySelectorAll(elements.UNDERLINE).length).toBe(1);
+    expect(doc.childNodes[0].childNodes[1].childNodes[1].childNodes[0].querySelector(elements.UNDERLINE).textContent).toBe('underline');
   });
 });
