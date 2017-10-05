545a1d9092ab37d52720245ed052b35a41a6c361
Adam Markon <amarkon895@gmail.com>
2017-10-05 11:02:56 -0400

    convert code to es5 for CRA compat (#30) (#31)

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const parseHTML = require('./lib/parseHTML');
 
 module.exports = {
   default: docsSoap,
-  docsSoap,
-  parseHTML
+  docsSoap: docsSoap, //eslint-disable-line
+  parseHTML: parseHTML //eslint-disable-line
 };
